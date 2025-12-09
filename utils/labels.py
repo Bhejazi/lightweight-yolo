4cc4f8f8f226d86f8003f41a4d05613e376e7765
Bardia Hejazi <151915799+Bhejazi@users.noreply.github.com>
2025-12-09 14:34:18 +0100

    Update labels.py

--- utils/labels.py
+++ utils/labels.py
@@ -4,14 +4,14 @@ Created on Mon Dec  8 14:58:32 2025
 
 @author: bhejazi
 """
-# Utilities to parse YOLO-format label files and to build training targets for the grid.
+# Utilities to parse YOLO-format label files and to build training targets for the grid
 
 from typing import List, Tuple
 import os
 
 def parse_yolo_label_file(txt_path: str) -> List[Tuple[float, float, float, float]]:
-    # Each line: class_id object_id x_center y_center width height (normalized [0,1]).
-    # We ignore class_id and object_id and return a list of (xc, yc, w, h).
+    # Each line: class_id object_id x_center y_center width height (normalized [0,1])
+    # We ignore class_id and object_id and return a list of (xc, yc, w, h)
     boxes = []
     with open(txt_path, "r") as f:
         for line in f:
@@ -27,9 +27,9 @@ def parse_yolo_label_file(txt_path: str) -> List[Tuple[float, float, float, floa
     return boxes
 
 def assign_to_grid(boxes: List[Tuple[float, float, float, float]], grid_size: int):
-    # Builds a (S, S, 5) target tensor from normalized boxes.
+    # Builds a (S, S, 5) target tensor from normalized boxes
     # For each box, mark responsible cell based on (xc, yc) and set:
-    #   objness=1, x, y, w, h = normalized absolute coords.
+    #   objness=1, x, y, w, h = normalized absolute coords
     import numpy as np
     target = np.zeros((grid_size, grid_size, 5), dtype=float)
     for (xc, yc, w, h) in boxes:
